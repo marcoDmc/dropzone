73c77b0703b54d3f952d583ac8df9a4eb2ac8fba
marcoDmc <marcodamasceno07@gmail.com>
2024-04-26 04:29:37 -0300

    chore:added next auth configuration

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,50 @@
+import { Http } from "@/config/axiosConfig"
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const nextAuthOptions: NextAuthOptions = {
+
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                name: { label: "name", type: "text", placeholder: "nickname" },
+                password: { label: "password", type: "password" },
+            },
+            async authorize(credentials) {
+                const { name, password } = credentials as {
+                    name: string;
+                    password: string;
+                };
+
+                const options = {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+
+                const data = {
+                    name,
+                    password
+                }
+                const res = await Http.post("/login", data, options)
+
+                const user = await res.data;
+
+                if (res && user) return user;
+
+                return null;
+
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/",
+    }
+}
+
+
+
+const handler = NextAuth(nextAuthOptions)
+
+export { handler as GET, handler as POST, nextAuthOptions }
