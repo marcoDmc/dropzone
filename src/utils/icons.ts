ff2d479ab3f62dde7b87e526d40327f23a758bce
marcoDmc <marcodamasceno07@gmail.com>
2024-05-10 13:33:52 -0300

    chore: I created a new file called 'icons.js' to centralize the import and export of all icons needed for the application. This will help keep the code more organized and modular, avoiding unnecessary repetitions and facilitating maintenance future. Icons can now be easily accessed and used anywhere in the code, promoting better separation of responsibilities.

---
+++ src/utils/icons.ts
@@ -0,0 +1,19 @@
+import { AiFillFileText } from "react-icons/ai";
+import { ImDownload } from "react-icons/im";
+import { IoCloudUpload, IoCloudUploadSharp } from "react-icons/io5";
+import { PiFilePdf } from "react-icons/pi";
+import { RiFileExcel2Line } from "react-icons/ri";
+import { BsKey, BsAt, BsFileEarmarkImage, BsFiletypeDoc } from "react-icons/bs";
+
+
+export default {
+    AiFillFileText: AiFillFileText,
+    BsFileEarmarkImage: BsFileEarmarkImage,
+    ImDownload: ImDownload,
+    IoCloudUpload: IoCloudUpload,
+    BsFiletypeDoc: BsFiletypeDoc,
+    IoCloudUploadSharp: IoCloudUploadSharp,
+    PiFilePdf: PiFilePdf, RiFileExcel2Line,
+    BsKey: BsKey,
+    BsAt: BsAt
+}
