d947c781033f5c4985a1a4a260f9f6bd8a7cd9d2
marcoDmc <marcodamasceno07@gmail.com>
2024-05-17 18:47:04 -0300

    feat: create component

--- src/components/FormComponent/Content.tsx
+++ src/components/FormComponent/Content.tsx
@@ -7,15 +7,18 @@ interface ContentProps {
 
 export default function Content({ children }: ContentProps) {
     return <>
-        <div 
-        className="
+        <div
+            className="
         h-full
         w-full
         grid
         gap-2
         "
         >
+
+
             {children}
+
         </div>
     </>
 }
