83e6aacb08bfe40e0c5e9277a84ab2810fab38dd
marcoDmc <marcodamasceno07@gmail.com>
2024-05-18 00:53:01 -0300

    fix: resolving next auth export error

---
+++ src/utils/next-auth.ts
@@ -0,0 +1,48 @@
+import { Http } from "@/app/config/axiosConfig";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const nextAuthOptions: NextAuthOptions = {
+
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                name: { label: "name", type: "text", placeholder: "nickname" },
+                password: { label: "password", type: "password" },
+            },
+            async authorize(credentials) {
+                const { name, password } = credentials as {
+                    name: string;
+                    password: string;
+                };
+
+                const options = {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+
+                const data = {
+                    name,
+                    password
+                }
+                const res = await Http.post("/login", data, options)
+
+                const user = await res.data;
+
+                if (res && user) return user;
+
+                return null;
+
+            },
+        }),
+    ],
+
+    pages: {
+        signIn: "/",
+    }
+}
+
+
+export { nextAuthOptions }
