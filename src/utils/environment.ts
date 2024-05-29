17ef27653a275aebd2864aa4d3b951b59356c1b9
marcoDmc <marcodamasceno07@gmail.com>
2024-05-29 09:44:49 -0300

    style: deleted file

--- src/utils/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    email: String(process.env.NEXT_PUBLIC_EMAIL),
-    password: String(process.env.NEXT_PUBLIC_PASSWORD),
-    nickname: String(process.env.NEXT_PUBLIC_NICKNAME)
-}
