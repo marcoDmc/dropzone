4c5ac2a0adb51872685120890bd4412e42b55d4b
marcodmc <marcodamasceno07@gmail.com>
2024-06-17 16:39:56 -0300

    refactor: refactoring code

--- src/app/(auth-routes)/v1/[token]/page.tsx
+++ src/app/(auth-routes)/v1/[token]/page.tsx
@@ -1,52 +1,38 @@
 'use client'
 
 import { useParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { motion } from "framer-motion"
+import { useEffect, useState } from "react";
 import { magicLink } from "@/service/magicLink";
+import { IVerifyMagicLinkDTO } from "@/types/IVerifyMagicLinkDTO";
 
 export default function urlTokenEmail() {
     const params = useParams()
     const router = useRouter()
+    const [credential, setCredential] = useState<IVerifyMagicLinkDTO>({ status: 0, message: "", data: "" })
 
     useEffect(() => {
         if (!params.token) return
 
         async function handleMagicLink() {
             const response = await magicLink.verifyMagicLink(String(params.token))
+            setCredential(prev => ({ ...prev, status: response.status, message: response.message, data: response.data }))
             setTimeout(() => {
-                if (response != 200) router.push("/")
+                if (response.status != 200) router.push("/")
                 else router.push("/change/password")
-            }, 10000);
+            }, 5000);
         }
         handleMagicLink()
     }, [])
 
     return (
         <>
-            <section className="bg-zinc-900 grid place-items-center justify-center h-screen w-full text-white">
-                <div className="bg-neutral-100 w-full max-w-44 h-44 rounded-full bg-gradient-to-r from-cyan-500 ...
-                     relative
-                     animate-spin
-                     after:absolute
-                     after:content-['']
-                     after:bg-zinc-900
-                     after:w-32
-                     after:h-32
-                     after:rounded-full
-                     after:top-6
-                     after:right-6
-                     before:absolute
-                     before:content-['']
-                     before:bg-zinc-900
-                     before:top-0
-                     before:w-14 
-                     before:h-24
-                     ">
-
+            <section className="bg-zinc-900 h-screen w-full items-center gap-16 justify-center flex flex-col">
+                <div className="bg-neutral-100 flex items-center justify-center w-full max-w-44 h-44  rounded-full bg-gradient-to-r from-cyan-500 ... relative  animate-spin">
+                    <div className=" bg-zinc-900 w-32 h-32 rounded-full"></div>
+                    <div className="absolute bg-zinc-900 w-16 h-28 top-0"></div>
                 </div>
-                <motion.h1 className="text-5xl capitalize" animate={{}}>
-                    please wait... </motion.h1>
+                <h1 className="text-4xl capitalize text-neutral-50">
+                    {!credential.data ? "please wait..." : credential.data}</h1>
             </section>
         </>
     )
