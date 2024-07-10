fbc01738a87d4f3a2b278cac9b57f58109bb2a5e
marcodmc <marcodamasceno07@gmail.com>
2024-07-10 07:55:43 -0300

    style: transforming input into a smaller part of the form component

--- src/components/FormComponent/Form.tsx
+++ src/components/FormComponent/Form.tsx
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import Content from "./Content";
+import { Password } from "./Password";
 import Root from "./Root";
 
 export const FormComponent = {
     Root: Root,
     Content: Content,
-    Button: Button
+    Button: Button,
+    Password:Password
 }
